9ca311ed078cf0197ad8a9feac632c9052db4578
spiltmiruku <spiltmiruku@gmail.com>
2020-03-11 20:07:36 -0600

    Changed Contact to be a class Component;

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import './contact.css';
+import React, { Component } from "react";
+import "./contact.css";
 
-const Contact = () => {
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
+  }
+  render() {
+      
+    const {
+        name,
+        email,
+        message
+      } = this.state;
+      
     return (
-        <div></div>
-    )
+      <div>
+        <h1>CONTACT US</h1>
+        <h2>We'd love to hear from you!</h2>
+
+        <input 
+        className="contact-input" 
+        type="text" 
+        placeholder='name'
+        value={name}
+        />
+        <input 
+        className="contact-input" 
+        type="email" 
+        placeholder='email'
+        value={email}
+        />
+        <input 
+        className="contact-input" 
+        type="text" 
+        placeholder='message...'
+        value={message}
+        />
+      </div>
+    );
+  }
 }
 
 export default Contact;
