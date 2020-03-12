ddd2e83bcf74e244100fd6dcaf3b53a579339de0
spiltmiruku <spiltmiruku@gmail.com>
2020-03-11 22:32:53 -0600

    Initial styling for portfolio page;

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import './portfolio.css';
+import React from "react";
+import "./portfolio.css";
+
+import laptop from "../../resources/laptop.png";
 
 const Portfolio = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <div>
+      <section className="client-projects-container">
+
+        <div className="project-wrapper">
+          <img id="project-img" src={laptop} alt="client project description" />
+          <div className='client-info'>
+          <span className="client-name">Company Name</span>
+          <br/>
+          <span className="client-type">tech startup</span>
+          </div>
+        </div>
+
+        <div className="project-wrapper">
+          <img id="project-img" src={laptop} alt="client project description" />
+          <div className='client-info'>
+          <span className="client-name">Company Name</span>
+          <br/>
+          <span className="client-type">tech startup</span>
+        </div>
+        </div>
+
+        <div className="project-wrapper">
+          <img id="project-img" src={laptop} alt="client project description" />
+          <div className='client-info'>
+          <span className="client-name">Company Name</span>
+          <br/>
+          <span className="client-type">tech startup</span>
+        </div>
+        </div>
+
+        <div className="project-wrapper">
+          <img id="project-img" src={laptop} alt="client project description" />
+          <div className='client-info'>
+          <span className="client-name">Company Name</span>
+          <br/>
+          <span className="client-type">tech startup</span>
+        </div>
+        </div>
+        
+      </section>
+    </div>
+  );
+};
 
 export default Portfolio;
