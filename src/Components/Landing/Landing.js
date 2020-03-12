da25b119cf765b5429b1c3d7e5b5adb43d41b31a
spiltmiruku <spiltmiruku@gmail.com>
2020-03-11 19:45:50 -0600

    Added some text an basic styling on Landing page;

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -6,11 +6,12 @@ const Landing = () => {
         <div>
             <h1 className='main-title'>Digital Solutions</h1>
 
-            <h2 className='sub-title'>We build websites & apps.</h2>
+            <h2 className='sub-title'>We build websites & apps</h2>
 
             <h3 className='catchy-slogan'>For startups. For businesses. For you.</h3>
 
-            <div className='learn-more-btn'>Learn More</div>
+            <div className='learn-more-btn'>
+                <span>Learn More</span></div>
         </div>
     )
 }
